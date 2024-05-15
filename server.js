cda3071cd9a819ec8dd7db214a096f96e5e4aae8
unknown <biel.vfo@gmail.com>
2024-05-15 17:50:10 -0300

    crud commit

--- server.js
+++ server.js
@@ -1,20 +1,7 @@
-import http from "http";
+import app from './src/app.js'
 
 const PORT = 3000; // porta para acessar o servidor
 
-const routes = { // cria rotas 
-    "/": "Curso de node.js!",
-    "/livros": "rota livro online",
-    "/autores": "rota livro autores online"
-};
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end(routes[req.url])
-});
-
-server.listen(PORT, () => {
-    console.log("servidor escutando!")
+app.listen(PORT, () => {
+    console.log("servidor escutando!");
 });
